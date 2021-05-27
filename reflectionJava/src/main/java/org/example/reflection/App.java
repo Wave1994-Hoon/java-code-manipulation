f56147b9ea84e3a5b0c3cc023d08f64efbd0c7f5
김광훈 <kk03187693@gmail.com>
2021-05-27 23:53:48 +0900

    add reflection exam about di

--- reflectionJava/src/main/java/org/example/reflection/App.java
+++ reflectionJava/src/main/java/org/example/reflection/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.reflection;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -15,7 +15,7 @@ public class App {
         Book book = new Book();
         Class<? extends Book> aClass = book.getClass();
 
-        Class<?> aClass1 = Class.forName("org.example.Book"); // FQCN 으로 인스턴스 생성 가능
+        Class<?> aClass1 = Class.forName("org.example.reflection.Book"); // FQCN 으로 인스턴스 생성 가능
 
         Arrays.stream(bookClass.getFields()).forEach(System.out::println); // 필드들 가져옴 (public만)
         Arrays.stream(bookClass.getDeclaredFields()).forEach(System.out::println); // 필드들 가져옴
@@ -32,7 +32,7 @@ public class App {
             });
         });
 
-        Class<?> bookClass2 = Class.forName("org.example.Book");
+        Class<?> bookClass2 = Class.forName("org.example.reflection.Book");
         Constructor<?> constructor = bookClass2.getConstructor(null);
         Book book1 = (Book) constructor.newInstance();
 
